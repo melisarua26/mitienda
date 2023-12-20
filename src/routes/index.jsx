5e7aeeb973c4284bc52aadf4bd98211751723cf0
melisarua26 <106440178+melisarua26@users.noreply.github.com>
2023-12-20 12:07:01 -0500

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        RECIPTORY- Repository por Recipes
+        <br />
+        <a>
+        </a>
+      </p>
+    );
+  }
